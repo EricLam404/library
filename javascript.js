cef5bac6b9af2b9229f3ee205399a6da354dcfa4
EricLam404 <lameric9888@gmail.com>
2022-08-28 14:15:47 -0400

    Added Add Book feature

--- javascript.js
+++ javascript.js
@@ -1,38 +1,64 @@
-let addingBooks = document.querySelector(".add");
-addingBooks.addEventListener('click', addBookToLibrary);
+let addBook = (e) => {
+  e.preventDefault();
+  let formData = new FormData(e.target);
+  console.log(formData);
+  let formProps = Object.fromEntries(formData);
+  console.log(formProps);
+  addBookToLibrary(formProps.title, formProps.author, formProps.pages);
+  displayBooks();
+  closeForm();
+  addingBooks.reset();
+}
+
+let addingBooks = document.getElementById("add-book-form");
+addingBooks.addEventListener("submit", addBook);
 
 let myLibrary = [];
 
-function Book(name, author) {
+displayBooks();
+
+function openForm() {
+  document.querySelector(".form-popup").style.display = "block";
+}
+
+function closeForm() {
+  document.querySelector(".form-popup").style.display = "none";
+}
+
+
+function Book(name, author, pages) {
   this.name = name;
   this.author = author;
+  this.pages = pages;
   
 }
 
 
-function addBookToLibrary(name, author) {
-  myLibrary.push(new Book(name, author));
+function addBookToLibrary(name, author, pages) {
+  myLibrary.push(new Book(name, author, pages));
+  console.log(myLibrary);
 }
-
 
 function displayBooks(){
   let body = document.body;
-  console.log(body);
   let container = document.querySelector(".container");
+  removeAllChildNodes(container);
   
   for(var i = 0; i < myLibrary.length; i++){
       let card = document.createElement("div");
       let name = document.createElement("div");
       let author = document.createElement("div");
+      let pages = document.createElement("div");
 
       name.textContent = myLibrary[i].name;
       author.textContent = myLibrary[i].author;
+      pages.textContent = myLibrary[i].pages;
 
       card.appendChild(name);
       card.appendChild(author);
+      card.appendChild(pages);
 
       card.classList.add("card");
-      console.log(card);
       container.appendChild(card);
 
   }
@@ -40,13 +66,8 @@ function displayBooks(){
   
 }
 
-displayBooks();
-
-function openForm() {
-  console.log(document.querySelector(".form-container"))
-  document.querySelector(".form-popup").style.display = "block";
-}
-
-function closeForm() {
-  document.querySelector(".form-popup").style.display = "none";
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
 }
