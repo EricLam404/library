52e530d0df887df8abf5cbab7d9fabd642b9aaf7
EricLam404 <lameric9888@gmail.com>
2022-08-29 21:15:06 -0400

    Added a checkbox for if the book is read

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ let addBook = (e) => {
   console.log(formData);
   let formProps = Object.fromEntries(formData);
   console.log(formProps);
-  addBookToLibrary(formProps.title, formProps.author, formProps.pages);
+  addBookToLibrary(formProps.title, formProps.author, formProps.pages, formProps.read ? true : false);
   displayBooks();
   closeForm();
   addingBooks.reset();
@@ -26,16 +26,17 @@ function closeForm() {
 }
 
 
-function Book(name, author, pages) {
+function Book(name, author, pages, isRead) {
   this.name = name;
   this.author = author;
   this.pages = pages;
+  this.isRead = isRead;
   
 }
 
 
-function addBookToLibrary(name, author, pages) {
-  myLibrary.push(new Book(name, author, pages));
+function addBookToLibrary(name, author, pages, isRead) {
+  myLibrary.push(new Book(name, author, pages, isRead));
   console.log(myLibrary);
 }
 
@@ -50,13 +51,26 @@ function displayBooks(){
       let author = document.createElement("div");
       let pages = document.createElement("div");
 
+      let readContainer = document.createElement("div");
+      let readName = document.createElement("div");
+      let read = document.createElement("input");
+      
+      read.type = "checkbox";
+      read.checked = myLibrary[i].isRead;
+      readName.textContent = "Is read?";
+      readContainer.classList.add("read-container");
+
       name.textContent = myLibrary[i].name;
       author.textContent = myLibrary[i].author;
       pages.textContent = myLibrary[i].pages;
 
+      readContainer.appendChild(read);
+      readContainer.appendChild(readName);
+
       card.appendChild(name);
       card.appendChild(author);
       card.appendChild(pages);
+      card.appendChild(readContainer);
 
       card.classList.add("card");
       container.appendChild(card);
